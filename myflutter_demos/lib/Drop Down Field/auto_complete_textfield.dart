0c5cc0dc1d961b72f98c15c664b2e0a75f14da85
harshad-rohin <harshad.rohin@gmail.com>
2021-10-30 15:05:03 +0530

    ROH-70 searchable dropdown poc completed

--- myflutter_demos/lib/Drop Down Field/auto_complete_textfield.dart
+++ myflutter_demos/lib/Drop Down Field/auto_complete_textfield.dart
@@ -17,42 +17,7 @@ class _AutoCTxtFieldState extends State<AutoCTxtField> {
   String newValue = '';
   final _txtFieldController = TextEditingController();
   List<StateModel> state = [
-    StateModel(0, 'Andhra Pradesh'),
-    StateModel(1, 'Arunachal Pradesh'),
-    StateModel(2, 'Assam'),
-    StateModel(3, 'Bihar'),
-    StateModel(4, 'Chhattisgarh'),
-    StateModel(5, 'Goa'),
-    StateModel(6, 'Gujarat'),
-    StateModel(7, 'Haryana'),
-    StateModel(8, 'Himachal Pradesh'),
-    StateModel(9, 'Jammu and Kashmir'),
-    StateModel(10, 'Jharkhand'),
-    StateModel(11, 'Karnataka'),
-    StateModel(12, 'Kerala'),
-    StateModel(13, 'Madhya Pradesh'),
     StateModel(14, 'Maharashtra'),
-    StateModel(15, 'Manipur'),
-    StateModel(16, 'Meghalaya'),
-    StateModel(17, 'Mizoram'),
-    StateModel(18, 'Nagaland'),
-    StateModel(19, 'Odisha'),
-    StateModel(20, 'Punjab'),
-    StateModel(21, 'Rajasthan'),
-    StateModel(22, 'Sikkim'),
-    StateModel(23, 'Tamil Nadu'),
-    StateModel(24, 'Telangana'),
-    StateModel(25, 'Tripura'),
-    StateModel(26, 'Uttarakhand'),
-    StateModel(27, 'Uttar Pradesh'),
-    StateModel(28, 'West Bengal'),
-    StateModel(29, 'Andaman and Nicobar Islands'),
-    StateModel(30, 'Chandigarh'),
-    StateModel(31, 'Dadra and Nagar Haveli'),
-    StateModel(32, 'Daman and Diu'),
-    StateModel(33, 'Delhi'),
-    StateModel(34, 'Lakshadweep'),
-    StateModel(35, 'Puducherry'),
   ];
 
   GlobalKey<AutoCompleteTextFieldState<StateModel>> key1 = GlobalKey();
@@ -97,8 +62,6 @@ class _AutoCTxtFieldState extends State<AutoCTxtField> {
 
               /// [itemFilter]
               itemFilter: (item, query) {
-                // print('item name: ${item.stateName}');
-                // print('query name: $query');
                 if (item.stateName != query){
                   enteredValue = query;
                 }
@@ -121,48 +84,29 @@ class _AutoCTxtFieldState extends State<AutoCTxtField> {
 
           ElevatedButton(
             onPressed: (){
-              int i;
               bool ch = false;
-              bool s = false;
               try{
-                //print(state[0].stateName);
-                for(i = 0; i < state.length; i++){
-                  ch = state[i].stateName == _txtFieldController.text;
+                for(var s in state){
+                  ch = s.stateName.toLowerCase() == _txtFieldController.text.toLowerCase();
                   if(ch == true){
                     print('true');
                     enteredValue = '';
                     newValue = '';
                     print('old value: ${_txtFieldController.text}');
-                    s = true;
-                    //break;
+                    break;
                   }
                 }
 
-                if(s == false && i == state.length){
+                if(ch == false){
                   print('if new value entered');
                   newValue = enteredValue;
                   state.add(StateModel(state.last.id + 1, newValue));
-                  newState.add(newValue);
-                  print(newState);
+                  for(var s in state){
+                    print(s.stateName);
+                  }
                   print('new value: $newValue');
                 }
 
-                // for (var item in state) {
-                //   if (item.stateName.contains(_txtFieldController.text)) {
-                //     print('if old value entered');
-                //     enteredValue = '';
-                //     newValue = '';
-                //     print('old value: ${_txtFieldController.text}');
-                //   }
-                //   else{
-                //     print('if new value entered');
-                //     newValue = enteredValue;
-                //     state.add(StateModel(state.last.id + 1, newValue));
-                //     newState.add(newValue);
-                //     print(newState);
-                //     print('new value: $newValue');
-                //   }
-                // }
               }
               catch(e){
                 print(e);
